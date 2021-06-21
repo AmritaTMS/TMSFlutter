fe432f9ca06b1560a047bc80d67a74bc5481645d
unknown <dhivakar189@gmail.com>
2021-06-21 14:24:49 +0530

    Login Screen 1.1.2

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,36 @@ class Root extends StatelessWidget {
     return MaterialApp(
       title: 'TMS | Timesheet Management System for Institutions',
       theme: ThemeData(
-        //need to add fonts
-        textTheme: TextTheme(
-          headline1: GoogleFonts.libreBaskerville(
-              fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-          headline2: GoogleFonts.libreBaskerville(
-              fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-          headline3: GoogleFonts.libreBaskerville(
-              fontSize: 48, fontWeight: FontWeight.w400),
-          headline4: GoogleFonts.libreBaskerville(
-              fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          headline5: GoogleFonts.libreBaskerville(
-              fontSize: 24, fontWeight: FontWeight.w400),
-          headline6: GoogleFonts.libreBaskerville(
-              fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-          subtitle1: GoogleFonts.libreBaskerville(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-          subtitle2: GoogleFonts.libreBaskerville(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-          bodyText1: GoogleFonts.roboto(
-              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-          bodyText2: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-          button: GoogleFonts.roboto(
-              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-          caption: GoogleFonts.roboto(
-              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-          overline: GoogleFonts.roboto(
-              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
-        ),
-        primarySwatch: Colors.blue,
-      ),
+          //need to add fonts
+          textTheme: TextTheme(
+            headline1: GoogleFonts.libreBaskerville(
+                fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+            headline2: GoogleFonts.libreBaskerville(
+                fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+            headline3: GoogleFonts.libreBaskerville(
+                fontSize: 48, fontWeight: FontWeight.w400),
+            headline4: GoogleFonts.libreBaskerville(
+                fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+            headline5: GoogleFonts.libreBaskerville(
+                fontSize: 24, fontWeight: FontWeight.w400),
+            headline6: GoogleFonts.libreBaskerville(
+                fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+            subtitle1: GoogleFonts.libreBaskerville(
+                fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+            subtitle2: GoogleFonts.libreBaskerville(
+                fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+            bodyText1: GoogleFonts.roboto(
+                fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+            bodyText2: GoogleFonts.roboto(
+                fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+            button: GoogleFonts.roboto(
+                fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+            caption: GoogleFonts.roboto(
+                fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+            overline: GoogleFonts.roboto(
+                fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
