1a14a222770b5dd5217fadfbd21f307fea8ddcf7
unknown <dhivakar189@gmail.com>
2021-06-23 14:08:06 +0530

    GraphQL CLients V1.2

--- lib/Helpers/GraphQL/graphql.dart
+++ lib/Helpers/GraphQL/graphql.dart
@@ -29,4 +29,44 @@ extension Graph on GraphQLClient {
       cacheRereadPolicy: CacheRereadPolicy.mergeOptimistic,
     ));
   }
+
+  Future queryCF(String query) {
+    final String readCharacter = query;
+    return this.query(QueryOptions(
+      document: gql(readCharacter),
+      fetchPolicy: FetchPolicy.cacheFirst,
+      errorPolicy: ErrorPolicy.all,
+      cacheRereadPolicy: CacheRereadPolicy.mergeOptimistic,
+    ));
+  }
+
+  Future mutationCF(String query) {
+    final String readCharacter = query;
+    return this.mutate(MutationOptions(
+      document: gql(readCharacter),
+      fetchPolicy: FetchPolicy.cacheFirst,
+      errorPolicy: ErrorPolicy.all,
+      cacheRereadPolicy: CacheRereadPolicy.mergeOptimistic,
+    ));
+  }
+
+  Future queryCN(String query) {
+    final String readCharacter = query;
+    return this.query(QueryOptions(
+      document: gql(readCharacter),
+      fetchPolicy: FetchPolicy.cacheAndNetwork,
+      errorPolicy: ErrorPolicy.all,
+      cacheRereadPolicy: CacheRereadPolicy.mergeOptimistic,
+    ));
+  }
+
+  Future mutationCN(String query) {
+    final String readCharacter = query;
+    return this.mutate(MutationOptions(
+      document: gql(readCharacter),
+      fetchPolicy: FetchPolicy.cacheAndNetwork,
+      errorPolicy: ErrorPolicy.all,
+      cacheRereadPolicy: CacheRereadPolicy.mergeOptimistic,
+    ));
+  }
 }
