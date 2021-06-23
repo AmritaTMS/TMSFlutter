84b34875d927081ab6c689fe4a38a5ab719440ff
unknown <dhivakar189@gmail.com>
2021-06-23 12:47:25 +0530

    GraphQL CLients V1.0

---
+++ lib/Helpers/GraphQL/auth_graphql.dart
@@ -0,0 +1,30 @@
+import 'package:graphql/client.dart';
+import 'package:tms/constants.dart';
+
+class AuthGraphQL {
+  final baseURL = url;
+  static late String _auth;
+
+  void setAuth(String value) {
+    _auth = value;
+  }
+
+  final _authLink = AuthLink(
+    getToken: () async => 'JWT ' + _auth,
+  );
+
+  GraphQLClient getClient() => GraphQLClient(
+        cache: GraphQLCache(),
+        link: _authLink.concat(HttpLink(baseURL)),
+      );
+}
+
+extension Graph on GraphQLClient {
+  Future queryA(String query) {
+    final String readCharacter = query;
+    return this.query(QueryOptions(
+      document: gql(readCharacter),
+      fetchPolicy: FetchPolicy.noCache,
+    ));
+  }
+}
